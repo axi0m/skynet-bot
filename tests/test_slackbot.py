fccb1411621ad592116b075f2805512bdd2890f1
Kevin Dienst <kevin.dienst@gmail.com>
2019-09-06 08:31:36 -0500

    Add test framework for slackbot

--- tests/test_slackbot.py
+++ tests/test_slackbot.py
@@ -5,4 +5,5 @@ from slackbot import verify_token
 def test_verify_token():
     token = verify_token()
     regex = '^xoxb-[0-9]{11}-[0-9]{12}-[a-zA-z0-9]{24}$'
-    assert re.match(regex, token) is not None
+    value = re.match(regex, token)
+    print(value)
